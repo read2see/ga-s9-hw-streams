08f708a516930f5ff3b7a9107df068bfff107a1c
Husain Habib <habib.h.e@outlook.com>
2025-12-01 19:31:05 +0300

    solve 2nd part of hw

--- src/CommonPrefix.java
+++ src/CommonPrefix.java
@@ -1,11 +1,36 @@
+import java.util.Arrays;
+
 public class CommonPrefix {
 
     public static String longestCommonPrefix(String[] words) {
-        return null;
+
+        if(words.length == 0){
+            return "";
+        }
+
+        if( words.length == 1 ){
+            return words[0];
+        }
+
+        Arrays.sort(words);
+
+        int prefixBoundary = Math.min(words[0].length(), words[words.length - 1].length());
+
+        for(int i = 0; i < prefixBoundary; i++){
+
+            if(words[0].charAt(i) != words[words.length - 1].charAt(i)){
+                return words[0].substring(0, i);
+            }
+        }
+
+        return words[0];
     }
 
     public static void main(String[] args) {
+        System.out.println(longestCommonPrefix(new String[]{}));
+        System.out.println(longestCommonPrefix(new String[]{"flower"}));
         System.out.println(longestCommonPrefix(new String[]{"flower", "flow", "flight"}));
         System.out.println(longestCommonPrefix(new String[]{"dog", "racecar", "car"}));
+        System.out.println(longestCommonPrefix(new String[]{"dog", "doggo", "dogggo"}));
     }
 }
